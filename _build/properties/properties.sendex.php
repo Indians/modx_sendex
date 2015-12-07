2535edfdbe0ab6aaec10a05ffcbc15375ab1f059
andrewsotnikov <a.sotnikov@libertyrus.ru>
2015-12-08 01:25:30 +0300

    Change processors Items on Newsletters

--- _build/properties/properties.sendex.php
+++ _build/properties/properties.sendex.php
@@ -5,7 +5,7 @@ $properties = array();
 $tmp = array(
 	'tpl' => array(
 		'type' => 'textfield',
-		'value' => 'tpl.Sendex.item',
+		'value' => 'tpl.Sendex.newsletter',
 	),
 	'sortby' => array(
 		'type' => 'textfield',
